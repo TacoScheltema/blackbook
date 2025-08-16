e07599bbce1f578e723c7a6acdd31f9d1b31d067
Taco Scheltema <taco@scheltema.org>
2025-08-16 14:23:09 +1000

    #29 fix from address

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com'] # From address for password resets
+    MAIL_FROM_ADDRESS = os.environ.get('MAIL_FROM_ADDRESS')
 
     # --- Password Reset ---
     PASSWORD_RESET_EXPIRATION_HOURS = int(os.environ.get('PASSWORD_RESET_EXPIRATION_HOURS', 24))
