a2f3fc93149ccc29ae3af2898c7103b378a940ad
Taco Scheltema <taco@scheltema.org>
2025-08-24 13:57:16 +1000

    #45 add GPLv3 License comments

--- app/email.py
+++ app/email.py
@@ -1,14 +1,33 @@
-from flask_mail import Message
-from app import mail
+# This file is part of Blackbook.
+#
+# Blackbook is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Blackbook is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Blackbook.  If not, see <https://www.gnu.org/licenses/>.
 from flask import current_app, render_template
+from flask_mail import Message
+
+from app import mail
+
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[Address Book] Reset Your Password',
-               sender=current_app.config['MAIL_FROM_ADDRESS'],
-               recipients=[user.email],
-               text_body=render_template('email/reset_password.txt', user=user, token=token),
-               html_body=render_template('email/reset_password.html', user=user, token=token))
+    send_email(
+        "[Address Book] Reset Your Password",
+        sender=current_app.config["MAIL_FROM_ADDRESS"],
+        recipients=[user.email],
+        text_body=render_template("email/reset_password.txt", user=user, token=token),
+        html_body=render_template("email/reset_password.html", user=user, token=token),
+    )
+
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
